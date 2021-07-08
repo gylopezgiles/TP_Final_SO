8767f8100ccdfd656e3b2ee2d0527d5857fad71d
Alan Camussi <alan.camussi@gmail.com>
2021-07-07 23:50:29 -0300

    Lógica de planificación con ejemplo de carga funcional

--- Proceso.py
+++ Proceso.py
@@ -3,22 +3,30 @@ import threading
 
 class Proceso(threading.Thread):
 
-	def __init__(self, tipo_proceso, tiempo_llegada, tiempo_ejecucion):
-		self.tipo_proceso = tipo_proceso
-		self.tiempo_llegada = tiempo_llegada
-		self.tiempo_ejecucion = tiempo_ejecucion
+    def __init__(self, tipo_proceso, tiempo_llegada, tiempo_ejecucion):
+        self.tipo_proceso = tipo_proceso
+        self.tiempo_llegada = tiempo_llegada
+        self.tiempo_ejecucion = tiempo_ejecucion
 
-	def run(self):
-		time.sleep(self.tiempo_ejecucion)
+    def run(self):
+        time.sleep(self.tiempo_ejecucion)
 
-	def leer_db(self):
-		time.sleep(self.tiempo_ejecucion)
+    def leer_db(self, i):
+        t = 0
+        while t < self.tiempo_ejecucion:
+            print("Lector " + str(i) + " está leyendo datos en la base")
+            time.sleep(2)
+            t += 1
 
-	def escribir_db(self):
-		time.sleep(self.tiempo_ejecucion)
+    def escribir_db(self, i):
+        t = 0
+        while t < self.tiempo_ejecucion:
+            print("Escritor " + str(2) + " está escribiendo datos en la base")
+            time.sleep(3)
+            t += 1
 
-	def es_lector(self):
-		return self.tipo_proceso.lower() == "lector"
+    def es_lector(self):
+        return self.tipo_proceso.lower() == "lector"
 
-	def es_escritor(self):
-		return self.tipo_proceso.lower() == "escritor"
+    def es_escritor(self):
+        return self.tipo_proceso.lower() == "escritor"
--- lectores_escritores.py
+++ lectores_escritores.py
@@ -1,45 +1,84 @@
 import time
 import threading
+import traceback
 import Proceso
+
+global orden_llegada, sem_db, mutex_cont_lectores, cont_lectores, instante
 
 orden_llegada = threading.Semaphore()
 sem_db = threading.Semaphore()
 mutex_cont_lectores = threading.Lock()
 cont_lectores = 0
 
-def ejecutar_lector(proceso):
-	orden_llegada.acquire()
-	mutex_cont_lectores.acquire()
-	if cont_lectores == 0:
-		sem_db.acquire()
-	cont_lectores += 1
-	orden_llegada.release()
-	mutex_cont_lectores.release()
-	proceso.leer_db()
-	mutex_cont_lectores.acquire()
-	cont_lectores -= 1
-	if cont_lectores == 0:
-		sem_db.release()
-	mutex_cont_lectores.release()
 
-def ejecutar_escritor(proceso):
-	orden_llegada.acquire()
-	sem_db.acquire()
-	orden_llegada.release()
-	proceso.escribir_db()
-	sem_db.release()
+def ejecutar_lector(proceso, i):
+    global cont_lectores
+    print("Lector " + str(i) + " esperando para leer")
+    orden_llegada.acquire()
+    mutex_cont_lectores.acquire()
+    if cont_lectores == 0:
+        sem_db.acquire()
+    cont_lectores += 1
+    orden_llegada.release()
+    mutex_cont_lectores.release()
 
-lector1 = Proceso.Proceso('lector', 0, 5)
-escritor1 = Proceso.Proceso('escritor', 1, 2)
-lector2 = Proceso.Proceso('lector', 1, 1)
-lector3 = Proceso.Proceso('lector', 2, 2)
-escritor2 = Proceso.Proceso('escritor', 1, 3)
+    proceso.leer_db(i)
+    print("Lector " + str(i) + " terminó de leer")
 
-procesos = []
-procesos.append(lector1)
-procesos.append(escritor1)
-procesos.append(lector2)
-procesos.append(lector3)
-procesos.append(escritor2)
-for proceso in procesos:
+    mutex_cont_lectores.acquire()
+    cont_lectores -= 1
+    if cont_lectores == 0:
+        sem_db.release()
+    mutex_cont_lectores.release()
 
+
+def ejecutar_escritor(proceso, i):
+    print("Escritor " + str(i) + " esperando para escribir")
+    orden_llegada.acquire()
+    sem_db.acquire()
+    orden_llegada.release()
+
+    proceso.escribir_db(i)
+    print("Escritor " + str(i) + " terminó de escribir")
+
+    sem_db.release()
+
+
+try:
+    # TODO Carga de prueba. Se leerá de un archivo e irá appendeando
+    procesos = [Proceso.Proceso('lector', 2, 2), Proceso.Proceso('lector', 2, 5), Proceso.Proceso('escritor', 3, 4),
+                Proceso.Proceso('lector', 6, 2), Proceso.Proceso('escritor', 8, 1)]
+    max_tiempo_llegada = 8
+
+    rindex = 1
+    windex = 1
+    for i in range(max_tiempo_llegada + 1):
+        print("---------- INSTANTE DE TIEMPO: " + str(i) + " ----------")
+        time.sleep(2)
+        for proceso in procesos:
+            if proceso.tiempo_llegada == i:
+                if proceso.es_lector():
+                    t = threading.Thread(target=ejecutar_lector, args=(proceso, rindex,))
+                    rindex += 1
+                    t.start()
+                else:
+                    t = threading.Thread(target=ejecutar_escritor, args=(proceso, windex,))
+                    windex += 1
+                    t.start()
+except:
+    print(str(traceback.format_exc()))
+    print("Error: unable to start thread")
+
+# lector1 = Proceso.Proceso('lector', 0, 5)
+# escritor1 = Proceso.Proceso('escritor', 1, 2)
+# lector2 = Proceso.Proceso('lector', 1, 1)
+# lector3 = Proceso.Proceso('lector', 2, 2)
+# escritor2 = Proceso.Proceso('escritor', 1, 3)
+#
+# procesos = []
+# procesos.append(lector1)
+# procesos.append(escritor1)
+# procesos.append(lector2)
+# procesos.append(lector3)
+# procesos.append(escritor2)
+# for proceso in procesos:
