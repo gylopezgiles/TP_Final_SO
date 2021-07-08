f9995b79ea3d0c119af80e5650757f5fbaf671cf
Gisela Lopez <glopezgiles@gmail.com>
2021-07-07 23:51:43 -0300

    hago que el proceso sea un thread

--- Proceso.py
+++ Proceso.py
@@ -4,12 +4,18 @@ import threading
 class Proceso(threading.Thread):
 
 	def __init__(self, tipo_proceso, tiempo_llegada, tiempo_ejecucion):
+		threading.Thread.__init__(self)
 		self.tipo_proceso = tipo_proceso
 		self.tiempo_llegada = tiempo_llegada
 		self.tiempo_ejecucion = tiempo_ejecucion
 
 	def run(self):
-		time.sleep(self.tiempo_ejecucion)
+		if self.es_lector():
+			print('proceso {} va a leer db'.format(self.getName()))
+			self.leer_db()
+		if self.es_escritor():
+			print('proceso {} va a escribir db'.format(self.getName()))
+			self.escribir_db()
 
 	def leer_db(self):
 		time.sleep(self.tiempo_ejecucion)
--- lectores_escritores.py
+++ lectores_escritores.py
@@ -1,4 +1,3 @@
-import time
 import threading
 import Proceso
 
@@ -8,38 +7,62 @@ mutex_cont_lectores = threading.Lock()
 cont_lectores = 0
 
 def ejecutar_lector(proceso):
+	print("ejecutando ejecutar_lector para proceso: {}".format(proceso.getName()))
+	global cont_lectores
 	orden_llegada.acquire()
 	mutex_cont_lectores.acquire()
 	if cont_lectores == 0:
+		print('proceso {} bloquea db'.format(proceso.getName()))
 		sem_db.acquire()
 	cont_lectores += 1
+	print('al ejecutar proceso {} contador lectores = {}'.format(proceso.getName(), cont_lectores))
 	orden_llegada.release()
 	mutex_cont_lectores.release()
-	proceso.leer_db()
+	print('proceso {} va a ejecutarse'.format(proceso.getName()))
+	proceso.run()
 	mutex_cont_lectores.acquire()
 	cont_lectores -= 1
+	print('luego de ejecutar proceso {} contador lectores = {}'.format(proceso.getName(), cont_lectores))
 	if cont_lectores == 0:
+		print('proceso {} libera db'.format(proceso.getName()))
 		sem_db.release()
 	mutex_cont_lectores.release()
 
 def ejecutar_escritor(proceso):
 	orden_llegada.acquire()
+	print('proceso {} bloquea db'.format(proceso.getName()))
 	sem_db.acquire()
 	orden_llegada.release()
-	proceso.escribir_db()
+	print('proceso {} va a ejecutarse'.format(proceso.getName()))
+	proceso.run()
 	sem_db.release()
+	print('proceso {} libera db'.format(proceso.getName()))
 
 lector1 = Proceso.Proceso('lector', 0, 5)
+lector1.setName("lector1")
 escritor1 = Proceso.Proceso('escritor', 1, 2)
+escritor1.setName("escritor1")
 lector2 = Proceso.Proceso('lector', 1, 1)
+lector2.setName("lector2")
 lector3 = Proceso.Proceso('lector', 2, 2)
+lector3.setName("lector3")
 escritor2 = Proceso.Proceso('escritor', 1, 3)
+escritor2.setName("escritor2")
+proceso = Proceso.Proceso('proceso', 1, 2)
+proceso.setName("proceso")
 
-procesos = []
-procesos.append(lector1)
-procesos.append(escritor1)
-procesos.append(lector2)
-procesos.append(lector3)
-procesos.append(escritor2)
+procesos = [lector1, escritor1, lector2, lector3, escritor2, proceso]
+
 for proceso in procesos:
+	if proceso.es_lector():
+		print('proceso {} es lector'.format(proceso.getName()))
+		ejecutar_lector(proceso)
+	else:
+		if proceso.es_escritor():
+			print('proceso {} es escritor'.format(proceso.getName()))
+			ejecutar_escritor(proceso)
+		else:
+			print('tipo proceso {} no identificado'.format(proceso.tipo_proceso))
+
+
 
